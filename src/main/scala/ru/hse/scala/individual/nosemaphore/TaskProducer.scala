e30d8fbb73e7e292b0275fcd997030932dd235e9
UsatovPavel <dbpfynbqcrfzbvgthbz@gmail.com>
2025-12-06 05:12:06 +0300

    Test semaphore

---
+++ src/main/scala/ru/hse/scala/individual/nosemaphore/TaskProducer.scala
@@ -0,0 +1,41 @@
+package ru.hse.scala.individual.nosemaphore
+
+import cats.effect._
+import cats.effect.std.{Console, Queue, Supervisor}
+import cats.implicits._
+import ru.hse.scala.individual.{FactorialAccumulator, ProcessMessage}
+//можно представить как loop в Task, но вынесен в класс для расширяемости архитектуры
+final class TaskProducer[F[_]: Async: Console](
+    queue: Queue[F, ProcessMessage],
+    workerSupervisor: Supervisor[F]
+) {
+
+  private val prompt  = Task.prompt
+  private val exitCmd = Task.exitCommand
+
+  def run: F[Unit] = loop
+
+  private def loop: F[Unit] =
+    Console[F].println(prompt) *>
+      Console[F].readLine.flatMap { text =>
+        text.trim match {
+          case t if t == exitCmd =>
+            Console[F].println("Exit")
+          case other =>
+            spawnWorker(other) >> loop
+        }
+      }
+
+  private def spawnWorker(text: String): F[Unit] =
+    workerSupervisor.supervise(
+      FactorialAccumulator.inputNumber(text, queue)
+    ).void
+}
+
+object TaskProducer {
+  def make[F[_]: Async: Console](
+      queue: Queue[F, ProcessMessage],
+      workerSupervisor: Supervisor[F]
+  ): F[TaskProducer[F]] =
+    Async[F].pure(new TaskProducer[F](queue, workerSupervisor))
+}
