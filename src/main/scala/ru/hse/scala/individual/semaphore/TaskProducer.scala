e62132d9244e1f988612196337af2df5a788c4ff
UsatovPavel <dbpfynbqcrfzbvgthbz@gmail.com>
2025-12-06 06:06:17 +0300

    Semaphore pakage

---
+++ src/main/scala/ru/hse/scala/individual/semaphore/TaskProducer.scala
@@ -0,0 +1,52 @@
+package ru.hse.scala.individual.semaphore
+
+import cats.effect._
+import cats.effect.implicits.monadCancelOps_
+import cats.effect.std.{Console, Queue, Semaphore, Supervisor}
+import cats.implicits._
+import ru.hse.scala.individual.{FactorialAccumulator, ProcessMessage}
+//можно представить как loop в Task, но вынесен в класс для расширяемости архитектуры
+final class TaskProducer[F[_]: Async: Console](
+    queue: Queue[F, ProcessMessage],
+    workerSupervisor: Supervisor[F],
+    sem: Semaphore[F]
+) {
+
+  private val prompt  = Task.prompt
+  private val exitCmd = Task.exitCommand
+
+  def run: F[Unit] = loop
+
+  private def loop: F[Unit] =
+    Console[F].println(prompt) *>
+      Console[F].readLine.flatMap { text =>
+        text.trim match {
+          case t if t == exitCmd =>
+            Console[F].println("Exit")
+          case other =>
+            spawnWorker(other) >> loop
+        }
+      }
+
+  private def spawnWorker(text: String): F[Unit] =
+    for {
+      _ <- sem.acquire
+      _ <- workerSupervisor.supervise(
+        FactorialAccumulator
+          .inputNumber(text, queue)
+          .guarantee(sem.release)
+      )
+    } yield ()
+}
+
+object TaskProducer {
+  val DEFAULT_MAX_PROCESS: Int = 10e5.toInt
+  def make[F[_]: Async: Console](
+      queue: Queue[F, ProcessMessage],
+      workerSupervisor: Supervisor[F],
+      maxParallel: Int = DEFAULT_MAX_PROCESS
+  ): F[TaskProducer[F]] =
+    Semaphore[F](maxParallel.toLong).map(sem =>
+      new TaskProducer[F](queue, workerSupervisor, sem)
+    )
+}
